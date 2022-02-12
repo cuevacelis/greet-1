b0e716ec389a5659d4c8017faa47d56907991e44
cuevacelis <cuevacelis@hotmail.com>
2022-02-11 22:05:14 -0500

    fix name in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   const name = req.query.nombre;
-  res.send(`<h1>${name}</h1>`);
+  res.send(`<h1>Hola ${name}!</h1>`);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));

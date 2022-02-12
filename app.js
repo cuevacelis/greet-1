9aa39c5bba8115a448447d86420a8678d0c6ac29
cuevacelis <cuevacelis@hotmail.com>
2022-02-11 22:04:08 -0500

    fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   const name = req.query.nombre;
-  res.send(name);
+  res.send(`<h1>${name}</h1>`);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
